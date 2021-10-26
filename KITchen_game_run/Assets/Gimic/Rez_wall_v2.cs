456a78b9c167eedc76afa6cca16849852b95d0dc
Fumi KASAI <s1F102002143@iniad.org>
2021-10-26 21:53:43 +0900

    透明スイッチ

---
+++ KITchen_game_run/Assets/Gimic/Rez_wall_v2.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rez_wall_v2 : MonoBehaviour
+{
+    private Switch Switch;
+    public int num;
+    private int check;
+    private Vector3 pos;
+    private Vector3 target;
+    public float moveX;
+    public float moveY;
+    public float sp = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(num);
+        this.Switch = FindObjectOfType<Switch>();
+        check = Switch.Move_select;
+        pos = this.gameObject.transform.position;
+        target = new Vector3(pos.x+moveX, pos.y+moveY,1);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Switch.ispush)
+        {
+            if (Switch.Move_select == num)
+            {
+                this.gameObject.transform.position = Vector3.MoveTowards(transform.position,target, Time.deltaTime*sp);
+            }
+        }
+    }
+}
